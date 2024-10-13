18575aaa46e0505a7b428fa285006aa0f632de80
vronnn <zhafrandzaky@gmail.com>
2024-10-13 23:01:03 +0700

    feat: integrate auth with backend

--- src/app/api/auth/[...nextauth]/route.api.ts
+++ src/app/api/auth/[...nextauth]/route.api.ts
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers';
 import type { AuthOptions } from 'next-auth';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
@@ -8,8 +9,16 @@ export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'Enter your email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Enter your password',
+        },
       },
       async authorize(credentials, _req) {
         if (credentials) {
@@ -27,13 +36,29 @@ export const authOptions: AuthOptions = {
   ],
   session: {
     strategy: 'jwt',
-    maxAge: 2 * 60 * 60,
   },
   secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   theme: {
     colorScheme: 'light',
     brandColor: '#415D9E',
     logo: 'https://github.com/user-attachments/assets/3b36741d-4a0a-4326-910b-2c13a97f02ab',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token = { ...user };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = { ...token };
+      return session;
+    },
+  },
+  events: {
+    signOut: () => {
+      cookies().delete('@bengcare/token');
+    },
   },
 };
 
--- src/middleware.api.ts
+++ src/middleware.api.ts
@@ -2,6 +2,10 @@ import type { NextMiddleware, NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { withAuth } from 'next-auth/middleware';
 
+import { baseURL } from '@/lib/api';
+import { UserProps } from '@/lib/user';
+import { ApiReturn } from '@/types/api';
+
 const middleware: NextMiddleware = (request: NextRequest) => {
   if (request.nextUrl.pathname.startsWith('/url'))
     return NextResponse.redirect(
@@ -10,16 +14,37 @@ const middleware: NextMiddleware = (request: NextRequest) => {
         'https://url.bengcare.com',
       ),
     );
+
   return NextResponse.next();
 };
 
 export default withAuth(middleware, {
   secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   callbacks: {
-    authorized({ req, token }) {
-      if (req.nextUrl.pathname.startsWith('/admin') && token === null) {
-        return false;
+    async authorized({ req }) {
+      if (req.nextUrl.pathname.startsWith('/admin')) {
+        const authToken = req.cookies.get('@bengcare/token')?.value;
+
+        if (!authToken) return false;
+
+        try {
+          const res = await fetch(baseURL + '/user/me', {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${authToken}`,
+            },
+          });
+
+          if (!res.ok) return false;
+
+          const user: ApiReturn<UserProps> = await res.json();
+
+          if (!user || (user && user.data.role != 'admin')) return false;
+        } catch (error) {
+          return false;
+        }
       }
+
       return true;
     },
   },
